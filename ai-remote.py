0934fe154adafd62e0af1d4cbee2daad7c47d501
Olivier Raginel <babar@klara.(none)>
2010-02-14 19:33:01 +0100

    Added support for GTK2 key specification and modifiers. Configured all keys, and removed the exit on snooze

--- ai-remote.py
+++ ai-remote.py
@@ -1,7 +1,9 @@
-#!/etc/bin/python
+#!/usr/bin/python
 import usb
 import virtkey
 from sys import exit
+import gtk
+from gtk.gdk import keyval_from_name
 
 class KeyMapper():
     def __init__(self):
@@ -14,10 +16,55 @@ class KeyMapper():
     
     def execute(self, remotenr):
         try:
-            self.v.press_keycode(self.map_dic[remotenr])
-            self.v.release_keycode(self.map_dic[remotenr])
+            #print str(remotenr)+' (pressed) mapped to '+str(self.map_dic[remotenr])
+            self.simulate_key(self.map_dic[remotenr])
         except KeyError:
             print str(remotenr)+' (not mapped)'
+
+    def simulate_key(self, keypressed):
+        """ simulate the key using python-virtkey
+            :param key: sent to keystroke_to_X11
+        """
+        if type(keypressed) == type(1):
+            self.v.press_keycode(keypressed)
+            self.v.release_keycode(keypressed)
+        else:
+            modifiers, key = self.keystroke_to_X11(keypressed)
+            if key:
+                if modifiers:
+                    self.v.lock_mod(modifiers)
+                try:
+                    self.v.press_keysym(key)
+                    self.v.release_keysym(key)
+                finally:
+                    if modifiers:
+                        self.v.unlock_mod(modifiers)
+            else:
+                    print "Could not decode keycode: "+keypressed
+    
+    def keystroke_to_X11(self, keystroke):
+        """ convert "CTRL+Shift+T" to (1<<2 | 1<<0, 28)
+            :param keystroke: The keystroke string.
+                             - can handle at least one 'real' key
+                             - only ctrl, shift and alt supported yet (case-insensitive)
+            :returns: tuple: (modifiers, keysym)
+        """
+        modifiers = 0
+        key = ""
+        splitted = keystroke.split("+")
+        for stroke in splitted:
+            lstroke = stroke.lower()
+            if lstroke == "ctrl" or lstroke == "control":
+                modifiers |= (1 << 2)
+            elif lstroke == "shift":
+                modifiers |= (1 << 0)
+            elif lstroke == "alt":
+                modifiers |= (1 << 3) # TODO: right?
+            else: # is a ordinary key (Only one)
+                key = gtk.gdk.keyval_from_name(stroke)
+        	if not key:
+                    print "Could not decode keycode: "+stroke
+        return (modifiers, key)
 
 #what busses are availible?
 busses = usb.busses()
@@ -38,18 +85,36 @@ if not ai:
     exit()
 
 #If we want our RC to to something, we should map its keys.
+# 1
+# 6  -  3 - 4
+# 12 - 13 - 5
+#
+#      7
+#  8 - 9 - 10
+#     11
 mapper = KeyMapper()
-mapper.map(4,121)   #Mute
-mapper.map(7,123)   #VolumeUp
-mapper.map(8,173)   #Previous
-mapper.map(9,172)   #Play/Pause
-mapper.map(10,171)  #Next
-mapper.map(11,122)  #VolumeDown
+mapper.map(1,124)   #Power # XF86PowerOff
+
+mapper.map(6,'Alt+Tab')   #Switch window
+mapper.map(3,'Left')   #AI gear # 15 s back
+mapper.map(4,121)   #Mute - snooze # XF86AudioMute
+
+mapper.map(12,'Shift+Left')  #AP-1 Trigger # 5 s back
+mapper.map(13,'Shift+Right')  #AP-2 Trigger # 5 s forward
+mapper.map(5,'Right')   #AP Menu # 1 min forward
+
+mapper.map(7,123)   #VolumeUp # XF86AudioRaiseVolume
+
+mapper.map(8,173)   #Previous # XF86AudioPrev
+mapper.map(9,172)   #Play/Pause # XF86AudioPlay
+mapper.map(10,171)  #Next # XF86AudioNext
+
+mapper.map(11,122)  #VolumeDown # XF86AudioLowerVolume
 
 #we're still there so we open the device
 handler = ai.open()
 
-#we know that there is only one confoguration
+#we know that there is only one configuration
 conf = ai.configurations[0]
 #only one interface
 iface = conf.interfaces[0][0]
@@ -76,5 +141,3 @@ while True:
     
     if data[1] != 0:
         mapper.execute(data[1])
-    if data[1] == 4:
-        break 
